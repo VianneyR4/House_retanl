a7c02b075ba9e6101c069667ddc328cd3222f983
VianneyR4 <vianneyrwicha2017@gmail.com>
2020-05-07 03:08:42 +0300

    Add the controllers folder

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+let index = require('../controllers/index');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', index.index);
 
 module.exports = router;
